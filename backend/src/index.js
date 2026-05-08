66cef1fc6c0d78ffe015d59e01617427d380e0d3
yasser8283 <yassershaikh99@gmail.com>
2026-05-08 15:03:06 +0530

    Fix deployment issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,9 +30,8 @@ app.use("/api/messages", messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    app.use((req, res) => {
+        res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
     });
 
 }
